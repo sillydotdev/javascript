ef4128fd2623f2ef2937609a40720a8ee563ec4a
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-11-02 14:48:04 +0000

    more about conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,12 +4,12 @@ let score = "33"
 //let score = "33dd"
 
 
-console.log(score);
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // 33 => 33
 // 33a => NaN (not a number)
@@ -21,7 +21,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let isLoggedInBoolean = Boolean(isLoggedIn)
-console.log(isLoggedInBoolean);
+//console.log(isLoggedInBoolean);
 
 // 1 => true; 0 => false
 // "" => false
@@ -32,5 +32,45 @@ console.log(isLoggedInBoolean);
 
 let someNumber = 33
 let someString = String(someNumber)
-console.log(someString);
-console.log(typeof(someString));
+// console.log(someString);
+// console.log(typeof(someString));
+
+
+// *******************  OPERATIONS  ***********************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(10%3);
+// console.log(10/2);
+
+let str1 = "hello"
+let str2 = " kamran"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2" + "3");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+// console.log(1 + "2" + "3");
+// console.log("1" + "2" + 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+let x = 100;
+// let y = x++   // out of y = 100
+let y = ++x      // output of y = 101
+console.log(y);
