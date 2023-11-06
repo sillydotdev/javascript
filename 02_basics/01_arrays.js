49a34650f69c0a9e5d4362309ec41825efbdd1c6
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-11-06 09:11:17 +0000

    basics of array in javascript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+// Arrays in javascript
+
+const myArray = [1, 2, 3, 4, 5]
+console.log(myArray);
+
+const heroes = ["Iron man", "Batman", "Hawk Eye"]
+const heroes1 = ['Iron man', 'Batman', 'Hawk Eye']
+console.log(heroes);
+console.log(heroes1);
+
+const myArray2 = new Array(1, 2, 3, 4, 5)
+console.log(myArray2);
+
+console.log(`the element at index 2 is: ${myArray[2]}`);
+
