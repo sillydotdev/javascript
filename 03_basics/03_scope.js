3b79dc9b538059b60dcbbf9451955debb0634473
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-11-07 11:50:52 +0000

    scope and hoisting

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -13,3 +13,49 @@ if(true) {
 console.log(a);
 console.log(b);
 // console.log(c);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+//scope in functions
+function one() {
+    const username = "Kamran"
+    function two() {
+        const age = 22
+        console.log(`The age of user ${username} is ${age}`);
+    }
+    // console.log(age);   // will give error as "age" scope is only within function "two"
+    two()
+}
+one()
+
+//Note: child function can use Parent function variables but vice-versa is not true
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//scope in conditional statements
+if(true) {
+    const username = "Kamran"
+    if(username === "Kamran"){
+        const wesite = " mkm.com"
+        console.log(username + wesite);
+    }
+    // console.log(website);  // error because out of scope
+}
+// console.log(username);    // error because out of scope
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++HOISTING++++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num) {     // here you can access function anytime, befor and after initialization
+    return num + 1
+}
+// console.log(addOne(5));
+
+
+
+// console.log(addTwo(5));          // here you cannot access function before initialiation
+const addTwo = function(num) {   // We can declare function in this way as well. We can hold it in a variable
+    return num + 2               // here it is usually called an Expression
+}
+console.log(addTwo(5));
