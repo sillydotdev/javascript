2bd06dfccc8054d21543793266d170da3fa4ba3a
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-11-10 10:51:03 +0000

    forEach loop in javascript

---
+++ 06_loops/05_forEach_loop.js
@@ -0,0 +1,50 @@
+// forEach loop 
+
+const languages = ["js", "java", "python", "swift", "rust"]
+languages.forEach( function(lang) {
+    // console.log(lang);
+    // console.log(languages.indexOf(lang));
+} )
+
+// forEach using arrow function
+const footballPlayers = ["Messi", "Neymar", "Pedri"]
+footballPlayers.forEach( (players) => {    // "players is a variable as a parameter which reperesents each element in the array"
+    // console.log(players);
+})
+
+
+// You can declare the function separately as well and use it in forEach loop
+function adition(print) {
+    // console.log(print);
+}
+languages.forEach(adition)
+
+
+// multiple parameters in forEach
+languages.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+})
+
+
+// Objects in an array
+// Iterating objects in an array
+const mylanguages = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+mylanguages.forEach( (lang) => {
+    console.log(lang.languageFileName);
+    console.log(lang.languageName);
+    console.log();
+} )
