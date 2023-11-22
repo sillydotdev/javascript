abbf616753eba4fda7b8f129a49341a4d0448fb7
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-11-22 11:41:04 +0000

    unlimited color chnaging project

--- 10_async_projects/01_UnlimitedColors/unlimitedColors.js
+++ 10_async_projects/01_UnlimitedColors/unlimitedColors.js
@@ -1,17 +1,34 @@
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+// function random(number) {
+//     return Math.floor(Math.random() * (number + 1));
+// }
+
+// function myBody() {
+//     const colorChanger = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+//     document.body.style.backgroundColor = colorChanger;
+// }
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = "#"
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+
+function myBody() {
+    // const colorChanger = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.body.style.backgroundColor = randomColor();
 }
 
 let unlimitedColor;
-
-function myBody() {
-    const colorChanger = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-    document.body.style.backgroundColor = colorChanger;
-}
-
 document.querySelector('#start').addEventListener('click', function(){
-    unlimitedColor = setInterval(myBody, 2000)
+    if(!unlimitedColor) {
+        unlimitedColor = setInterval(myBody, 1000)
+    }
 })
 document.querySelector('#stop').addEventListener('click', function(){
     clearInterval(unlimitedColor)
+    unlimitedColor = null;
 })
